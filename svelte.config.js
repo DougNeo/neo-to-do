d0d9a971c005582f64bf932614e5d14e93a13d09
Douglas Silva Santos <doug.neo@gmail.com>
2025-02-03 09:26:12 -0300

    Deployando o site

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  preprocess: vitePreprocess(),
 
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: '404.html' // Adicione esta linha para rotas dinâmicas
+    }),
+    paths: {
+      // Ajuste para o nome do seu repositório GitHub
+      base: process.env.NODE_ENV === 'production' ? '/neo-to-do' : '' 
+    }
+  }
 };
 
 export default config;
